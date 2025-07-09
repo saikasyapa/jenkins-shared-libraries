ee612b0a6cba63bddbe76efad5b45431f24c6680
saikasyapa <146060560+saikasyapa@users.noreply.github.com>
2025-07-09 18:38:27 +0530

    Update build.groovy - 1

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,5 @@
 def call(String ProjectName) {
   echo "This stage is for building the image."
   sh "docker build -t ${ProjectName} ."
-  echo "Image builded successfully."
+  echo "Image builded successfullyyy."
 }
